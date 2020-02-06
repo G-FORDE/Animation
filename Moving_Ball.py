1d79ab9b4189bd05d53c89bf47b8ab6527427b7c
G-FORDE <gregoryforde1984@gmail.com>
2020-02-06 10:36:33 -0500

    First commit

---
+++ Moving_Ball.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+
+tk = Tk()
+
+#create the canvas
+canvas = Canvas(tk)
+canvas.pack(fill=BOTH, expand=1)
+
+#create the ball
+ball = canvas.create_oval(20, 20, 50, 50, fill="blue")
+
+#create coordinate points where to move the ball
+def animation(x_move, y_move):
+    canvas.move(ball, x_move, y_move)
+    canvas.update()
+
+    #speed of the ball
+    canvas.after(100)
+
+    #create the animation
+    tk.after_idle(animation, x_move, y_move)
+
+#controle the direction
+animation(4, 4)
+
+
+tk.mainloop()
